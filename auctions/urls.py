86e7418703c9418ec397da2ab61232a7c6fd3cf4
juliusriteco <62749351+juliusriteco@users.noreply.github.com>
2024-10-05 20:02:52 +0200

    bijna klaar

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from . import views
 
@@ -6,5 +9,12 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
-]
+    path("register", views.register, name="register"),
+    path("create", views.createlisting, name="create"),
+    path('product_details/<int:product_details_id>/', views.product_details, name='product_details'),
+    path('product/<int:product_id>/bid/', views.New_bid, name='New_bid'),
+    path("product/<int:product_id>/watchlist/", views.toggle_watchlist, name="toggle_watchlist"),
+    path("product/<int:product_id>/disable/", views.disable, name="disable"),
+
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
